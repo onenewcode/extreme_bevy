63d01aab8d12a642f9b966e6469bc89242288e21
29071 <lovestudy@qq.com>
2024-10-29 12:10:45 +0800

    更改为官方的目录结构

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
-use bevy::{prelude::*, render::camera::ScalingMode, tasks::IoTaskPool, utils::HashMap};
-use bevy_ggrs::*;
-use bevy_matchbox::{prelude::{PeerId, SingleChannel}, MatchboxSocket};
-const INPUT_UP: u8 = 1 << 0;
-const INPUT_DOWN: u8 = 1 << 1;
-const INPUT_LEFT: u8 = 1 << 2;
-const INPUT_RIGHT: u8 = 1 << 3;
-const INPUT_FIRE: u8 = 1 << 4;
-
-// The first generic parameter, u8, is the input type: 4-directions + fire fits
-// easily in a single byte
-// The second parameter is the address type of peers: Matchbox' WebRtcSocket
-// addresses are called `PeerId`s
-type Config = bevy_ggrs::GgrsConfig<u8, PeerId>;
-#[derive(Component)]
-struct Player {
-    handle: usize
-}
+use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy_ggrs::{ggrs, AddRollbackCommandExtension, GgrsApp, GgrsPlugin, PlayerInputs, ReadInputs};
+use bevy_matchbox::prelude::*;
+use components::Player;
+use input::{direction, read_local_inputs};
+mod input;
+mod components;
+pub type Config = bevy_ggrs::GgrsConfig<u8, PeerId>;
 fn main() {
     App::new()
     .add_plugins((
@@ -31,12 +21,12 @@ fn main() {
         }),
         GgrsPlugin::<Config>::default(), // NEW
     ))
-    .rollback_component_with_clone::<Transform>() // NEW
+    .rollback_component_with_clone::<Transform>() 
     .insert_resource(ClearColor(Color::srgb(0.53, 0.53, 0.53)))
-    .add_systems(Startup, (setup, spawn_players, start_matchbox_socket))
+    .add_systems(Startup, (setup, spawn_player, start_matchbox_socket))
     .add_systems(Update, wait_for_players) // CHANGED
     .add_systems(ReadInputs, read_local_inputs) // NEW
-    .add_systems(GgrsSchedule, move_players) // NEW
+    .add_systems(bevy_ggrs::GgrsSchedule, move_players) // NEW
     .run();
 }
 fn setup(mut commands: Commands) {
@@ -44,143 +34,99 @@ fn setup(mut commands: Commands) {
     camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(10.);
     commands.spawn(camera_bundle);
 }
-fn spawn_players(mut commands: Commands) {
-    // Player 1
-    commands
-        .spawn((
-            Player { handle: 0 },
-            SpriteBundle {
-                transform: Transform::from_translation(Vec3::new(-2., 0., 0.)),
-                sprite: Sprite {
-                    color: Color::srgb(0., 0.47, 1.),
-                    custom_size: Some(Vec2::new(1., 1.)),
-                    ..default()
-                },
-                ..default()
-            },
-        ))
-        .add_rollback();
-
-    // Player 2
-    commands
-        .spawn((
-            Player { handle: 1 },
-            SpriteBundle {
-                transform: Transform::from_translation(Vec3::new(2., 0., 0.)),
-                sprite: Sprite {
-                    color: Color::srgb(0., 0.4, 0.),
-                    custom_size: Some(Vec2::new(1., 1.)),
-                    ..default()
-                },
-                ..default()
-            },
-        ))
-        .add_rollback();
-}
-fn wait_for_players(mut commands: Commands, mut socket: ResMut<MatchboxSocket<SingleChannel>>) {
-    if socket.get_channel(0).is_err() {
-        return; // we've already started
-    }
-
-    // Check for new connections
-    socket.update_peers();
-    let players = socket.players();
-
-    let num_players = 2;
-    if players.len() < num_players {
-        return; // wait for more players
-    }
-
-    info!("All peers have joined, going in-game");
-
-    // create a GGRS P2P session
-    let mut session_builder = ggrs::SessionBuilder::<Config>::new()
-        .with_num_players(num_players)
-        .with_input_delay(2);
-
-    for (i, player) in players.into_iter().enumerate() {
-        session_builder = session_builder
-            .add_player(player, i)
-            .expect("failed to add player");
-    }
-
-    // move the channel out of the socket (required because GGRS takes ownership of it)
-    let channel = socket.take_channel(0).unwrap();
-
-    // start the GGRS session
-    let ggrs_session = session_builder
-        .start_p2p_session(channel)
-        .expect("failed to start session");
-
-    commands.insert_resource(bevy_ggrs::Session::P2P(ggrs_session));
-}
-fn move_players(
-    mut players: Query<(&mut Transform, &Player)>,
-    inputs: Res<PlayerInputs<Config>>,
-    time: Res<Time>,
-) {
-    for (mut transform, player) in &mut players {
-        let (input, _) = inputs[player.handle];
-
-        let mut direction = Vec2::ZERO;
-
-        if input & INPUT_UP != 0 {
-            direction.y += 1.;
-        }
-        if input & INPUT_DOWN != 0 {
-            direction.y -= 1.;
-        }
-        if input & INPUT_RIGHT != 0 {
-            direction.x += 1.;
-        }
-        if input & INPUT_LEFT != 0 {
-            direction.x -= 1.;
-        }
-        if direction == Vec2::ZERO {
-            continue;
-        }
-
-        let move_speed = 7.;
-        let move_delta = direction * move_speed * time.delta_seconds();
-        transform.translation += move_delta.extend(0.);
-    }
-}
-
-/// 通过 matchbox 连接到服务器，与其他用户建立连接进行对战
+// 建立连接到 Matchbox 服务器
 fn start_matchbox_socket(mut commands: Commands) {
-    // ?前的参数是游戏，参数后面的事房间号
     let room_url = "ws://127.0.0.1:3536/extreme_bevy?next=2";
     info!("connecting to matchbox server: {room_url}");
     commands.insert_resource(MatchboxSocket::new_ggrs(room_url));
 }
-fn read_local_inputs(
-    mut commands: Commands,
-    keys: Res<ButtonInput<KeyCode>>,
-    local_players: Res<LocalPlayers>,
+
+pub fn spawn_player(mut commands: Commands) {
+    // Player 1
+    commands
+    .spawn((
+       Player { handle: 0 },
+        SpriteBundle {
+            transform: Transform::from_translation(Vec3::new(-2., 0., 0.)),
+            sprite: Sprite {
+                color: Color::srgb(0., 0.47, 1.),
+                custom_size: Some(Vec2::new(1., 1.)),
+                ..default()
+            },
+            ..default()
+        },
+    ))
+    .add_rollback();
+
+// Player 2
+commands
+    .spawn((
+        Player { handle: 1 },
+        SpriteBundle {
+            transform: Transform::from_translation(Vec3::new(2., 0., 0.)),
+            sprite: Sprite {
+                color: Color::srgb(0., 0.4, 0.),
+                custom_size: Some(Vec2::new(1., 1.)),
+                ..default()
+            },
+            ..default()
+        },
+    ))
+    .add_rollback();
+}
+ fn wait_for_players(mut commands: Commands, mut socket: ResMut<MatchboxSocket<SingleChannel>>) {
+   if socket.get_channel(0).is_err() {
+       return; // we've already started
+   }
+
+   // Check for new connections
+   socket.update_peers();
+   let players = socket.players();
+
+   let num_players = 2;
+   if players.len() < num_players {
+       return; // wait for more players
+   }
+
+   info!("All peers have joined, going in-game");
+
+   // create a GGRS P2P session
+   let mut session_builder = ggrs::SessionBuilder::<Config>::new()
+       .with_num_players(num_players)
+       .with_input_delay(2);
+
+   for (i, player) in players.into_iter().enumerate() {
+       session_builder = session_builder
+           .add_player(player, i)
+           .expect("failed to add player");
+   }
+
+   // move the channel out of the socket (required because GGRS takes ownership of it)
+   let channel = socket.take_channel(0).unwrap();
+
+   // start the GGRS session
+   let ggrs_session = session_builder
+       .start_p2p_session(channel)
+       .expect("failed to start session");
+
+   commands.insert_resource(bevy_ggrs::Session::P2P(ggrs_session));
+}
+ fn move_players(
+   mut players: Query<(&mut Transform, &Player)>,
+   inputs: Res<PlayerInputs<Config>>,
+   time: Res<Time>,
 ) {
-    let mut local_inputs = HashMap::new();
+   for (mut transform, player) in &mut players {
+       let (input, _) = inputs[player.handle];
 
-    for handle in &local_players.0 {
-        let mut input = 0u8;
+       let direction = direction(input);
 
-        if keys.any_pressed([KeyCode::ArrowUp, KeyCode::KeyW]) {
-            input |= INPUT_UP;
-        }
-        if keys.any_pressed([KeyCode::ArrowDown, KeyCode::KeyS]) {
-            input |= INPUT_DOWN;
-        }
-        if keys.any_pressed([KeyCode::ArrowLeft, KeyCode::KeyA]) {
-            input |= INPUT_LEFT
-        }
-        if keys.any_pressed([KeyCode::ArrowRight, KeyCode::KeyD]) {
-            input |= INPUT_RIGHT;
-        }
-        if keys.any_pressed([KeyCode::Space, KeyCode::Enter]) {
-            input |= INPUT_FIRE;
-        }
+       if direction == Vec2::ZERO {
+           continue;
+       }
 
-        local_inputs.insert(*handle, input);
-    }
-
-    commands.insert_resource(LocalInputs::<Config>(local_inputs));
+       let move_speed = 7.;
+       let move_delta = direction * move_speed * time.delta_seconds();
+       transform.translation += move_delta.extend(0.);
+   }
 }
